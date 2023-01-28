4d0336fca8b10225c10c521121fb708c15e441f8
Boi Soth <cambodianjesuss@gmail.com>
2023-01-28 12:22:37 -0800

    setting up auth route for login and middleware auth controller for login

--- index.js
+++ index.js
@@ -8,7 +8,8 @@ import helmet from 'helmet';
 import morgan from 'morgan';
 import path from 'path';
 import { fileURLToPath } from 'url';
-import { register } from './controllers/auth.js';
+import authRoutes from './routes/auth'
+import { register } from './controllers/auth';
 
 /* CONFIGURATIONS */
 const __filename = fileURLToPath(import.meta.url);
@@ -38,6 +39,8 @@ const upload = multer({ storage });
 /* ROUTES WITH FILES */
 app.post('/auth/register', upload.single('picture'), register)
 
+/* ROUTES */
+app.use('/auth', authRoutes);
 
 /* MONGOOSE SETUP */
 const PORT = process.env.PORT || 6001;
