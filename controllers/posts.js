c2ed0de88fe05c97c1695baaa3d22d5fa330fc32
Boi Soth <cambodianjesuss@gmail.com>
2023-01-29 02:25:25 -0800

    created updated post likes from the id and checked for userId if liked or not, then updated the database property of likes

--- controllers/posts.js
+++ controllers/posts.js
@@ -48,3 +48,31 @@ export const getUserPosts = async (req, res) => {
     res.status(404).json({message: err.message});
   }
 }
+
+/* UPDATE */
+export const likePost = async (req, res) => {
+  try{
+    const { id } = req.params;
+    const { userId } = req.body;
+    const post = await Post.findById(id);
+
+    // liked by this user if found, delete like or set true
+    const isLiked = post.likes.get(userId); 
+    if(isLiked){
+      post.likes.delete(userId);
+    } else {
+      post.likes.set(userId, true)
+    }
+
+    // Update database
+    const updatedPost = await Post.findByIdAndUpdate(
+      id,
+      { likes: post.likes},
+      { new: true}
+    );
+
+    res.status(200).json(updatedPost)
+  }catch(err){
+    res.status(404).json({message: err.message});
+  }
+}
